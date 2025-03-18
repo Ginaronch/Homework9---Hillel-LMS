c0f539429a8bf2fbc20141e57f739d09b463f394
Ginaronch <alexandr.yarosh1@gmail.com>
2025-03-18 12:12:13 +0100

    version 1.0

--- 9.2.py
+++ 9.2.py
@@ -4,7 +4,7 @@ def difference(*args):
     else:
         max_number = max(args)
         min_number = min(args)
-        return round(max_number - min_number, 2) #число, количество знаков,которое мы хотим оставить после запятой(если значение отрицательное,то перед запятой)
+        return round(max_number - min_number, 2) #число, количество знаков,которое мы хотим оставить после запятой(если значение отрицательное,то перед запятой(все числа заменятся на 0))
 assert difference(1, 2, 3) == 2
 assert difference(5, -5) == 10
 assert difference(10.2, -2.2, 0, 1.1, 0.5) == 12.4
